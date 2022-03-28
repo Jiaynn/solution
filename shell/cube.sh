00de48f2085c75d9668239999d1f5306fcc95c4b
Spencer17x <1253478653@qq.com>
2022-03-28 16:51:03 +0800

    refactor: 修改cube shell命令

--- shell/cube.sh
+++ shell/cube.sh
@@ -1,14 +1,23 @@
-rm -rf packages/sdk &&
-rm -rf packages/demo/qnuniapp-im-demo &&
-rm -rf packages/demo/qnweapp-im-demo &&
-rm -rf packages/demo/qnweb-im-demo &&
-rm -rf packages/demo/qnweb-rtc-ai-demo &&
-rm -rf packages/demo/qnweb-whiteboard-demo &&
+sdk_dir=packages/sdk
+demo_dir=packages/demo
+
+rm -rf ${sdk_dir}/qnuniapp-im \
+${sdk_dir}/qnweapp-im  \
+${sdk_dir}/qnweb-exam-sdk \
+${sdk_dir}/qnweb-im \
+${sdk_dir}/qnweb-rtc-ai \
+${sdk_dir}/qnweb-whiteboard \
+${sdk_dir}/whiteboard \
+${demo_dir}/qnuniapp-im-demo \
+${demo_dir}/qnweapp-im-demo \
+${demo_dir}/qnweb-im-demo \
+${demo_dir}/qnweb-rtc-ai-demo \
+${demo_dir}/qnweb-whiteboard-demo &&
 
 cp cube-template/** . &&
 
-rm -rf docs &&
-rm -rf cube-template &&
-rm -rf scripts &&
-rm -rf shell
+rm -rf docs \
+cube-template \
+scripts \
+shell
 
