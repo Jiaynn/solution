5de38de760c65ba049133bcbb3d6cce489a7fedb
Spencer17x <1253478653@qq.com>
2022-05-07 17:36:15 +0800

    docs(qnweb-whiteboard-demo): 文档更新

--- shell/copy_whiteboard_sdk.sh
+++ shell/copy_whiteboard_sdk.sh
@@ -2,4 +2,5 @@ source_dir=packages/sdk/qnweb-whiteboard
 target_dir=packages/demo/qnweb-whiteboard-demo
 
 cp ${source_dir}/build/*.umd.js ${target_dir}/public &&
+cp ${source_dir}/README.md ${target_dir}/SDK.md &&
   echo "copy_white_board_sdk success"
