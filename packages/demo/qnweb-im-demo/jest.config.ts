6f4c88338ced282a3f999e89358897eaf032c4d5
Spencer17x <1253478653@qq.com>
2022-05-09 10:42:03 +0800

    feat(qnweb-im-demo): submodule

--- packages/demo/qnweb-im-demo/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from '@jest/types';
-
-export default async (): Promise<Config.InitialOptions> => {
-  return {
-    verbose: true,
-    preset: 'ts-jest'
-    // testEnvironment: 'node',
-  };
-};
