7609818bbf871cf20d2dd82d092b02721f01c971
Spencer17x <1253478653@qq.com>
2022-06-21 19:05:04 +0800

    chore(qnweb-rtc-ai): 文档整理

--- shell/copy_rtc_ai.sh
+++ shell/copy_rtc_ai.sh
@@ -1,6 +1,6 @@
-source_dir=packages/qnweb-rtc-ai
-target_dir=packages/qnweb-rtc-ai-demo
+sdk_dir=packages/qnweb-rtc-ai
+demo_dir=packages/qnweb-rtc-ai-demo
 
-cp ${source_dir}/build/qnweb-rtc-ai.*.js ${target_dir}/src/sdk &&
- cp ${source_dir}/*.md ${target_dir} &&
+cp ${sdk_dir}/build/qnweb-rtc-ai.*.js ${demo_dir}/public/sdk &&
+cp -r ${sdk_dir}/documents ${demo_dir} &&
  echo "copy_rtc_ai success"
