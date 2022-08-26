cf99f9fe162e74c08fda5d71a00295d458f97f8c
Spencer17x <1253478653@qq.com>
2022-08-26 15:44:32 +0800

    feat(qnweb-whiteboard-demo): 新增创建房间、加入房间表单弹窗选择

--- packages/qnweb-whiteboard-demo/vite.config.ts
+++ packages/qnweb-whiteboard-demo/vite.config.ts
@@ -1,13 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 const pkg = require('./package.json');
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  build: {
-    outDir: `dist/${pkg.version}`,
-  }
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  console.log('当前环境: ', process.env.VITE_NODE_ENV);
+
+  return {
+    plugins: [react()],
+    base: './',
+    build: {
+      rollupOptions: {
+        output: [
+          { dir: `dist/${pkg.version}` },
+          { dir: 'dist' }
+        ]
+      }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    define: {
+      mainVersion: JSON.stringify(pkg.version)
+    }
+  };
 });
