bcb362d7b1f9b752f0b18c1032b7eecb0bb06074
Spencer17x <1253478653@qq.com>
2022-11-16 11:33:00 +0800

    chore(qnweb-vr-player-demo): 修改gulpfile配置

--- packages/qnweb-vr-player-demo/gulpfile.js
+++ packages/qnweb-vr-player-demo/gulpfile.js
@@ -1,20 +1,45 @@
-var gulp = require('gulp');
-var changed = require('gulp-changed');
-var uglify = require('gulp-uglify');
-var gulpSequence = require('gulp-sequence');
+const gulp = require('gulp');
+const changed = require('gulp-changed');
+const uglify = require('gulp-uglify');
+const rimraf = require('rimraf');
+const execSync = require('child_process').execSync;
 
-var SRC = 'src/';
-var DEST = 'dist/js';
+const SRC = 'src/';
+const DEST = 'dist/js';
 
-gulp.task('dist', function () {
-    return gulp.src(SRC + '*.js')
-        .pipe(changed(DEST))
-        .pipe(uglify({
-            mangle: true,
-            compress: true,
-        }))
-        .on('error', function (err) {
-            console.log(err.toString());
-        })
-        .pipe(gulp.dest(DEST));
+gulp.task('build', cb => {
+  gulp.series('clean', 'libs', 'examples', 'html')(cb);
+});
+
+gulp.task('dev', () => {
+  gulp.watch(['examples', 'index.html'], cb => {
+    gulp.series('examples', 'html')(cb);
+  });
+});
+
+gulp.task('libs', () => {
+  return gulp.src(SRC + '*.js')
+    .pipe(changed(DEST))
+    .pipe(uglify({
+      mangle: true,
+      compress: true,
+    }))
+    .on('error', function (err) {
+      console.log(err.toString());
+    })
+    .pipe(gulp.dest(DEST));
+});
+
+gulp.task('clean', cb => {
+  rimraf('dist', cb);
+});
+
+gulp.task('examples', cb => {
+  execSync('cp -r examples dist', { stdio: 'inherit' });
+  cb();
+});
+
+gulp.task('html', cb => {
+  execSync('cp -r index.html dist', { stdio: 'inherit' });
+  cb();
 });
