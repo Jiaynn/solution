43ddadf26964b661e231d485a1a536bc35270413
Spencer17x <1253478653@qq.com>
2023-02-21 15:57:52 +0800

    fix(portal-solution): 图片管理逻辑优化

--- portal/front/portal-solution/src/components/ImageManagement/index.tsx
+++ portal/front/portal-solution/src/components/ImageManagement/index.tsx
@@ -4,6 +4,8 @@ import { useInjection } from 'qn-fe-core/di'
 
 import { observer } from 'mobx-react'
 
+import { Empty, Loading } from 'react-icecream-2'
+
 import SelectBucket from 'components/Configuration/SelectBucket'
 
 import { ObjectManage } from 'kodo/components/BucketDetails/ObjectManage'
@@ -12,11 +14,14 @@ import { MediaStyleDrawer } from 'kodo/components/BucketDetails/MediaStyle/Creat
 import { MediaStyleDrawerStore } from 'kodo/components/BucketDetails/MediaStyle/CreateStyle/common/Drawer/store'
 import { BucketStore } from 'kodo/stores/bucket'
 
+import './style.less'
+
 export default observer(function ImageManagement() {
   const mediaStyleStore = useInjection(MediaStyleDrawerStore)
   const bucketStore = useInjection(BucketStore)
 
   const [selectedBucketName, setSelectedBucketName] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const onChange = useCallback((value: string) => {
     setSelectedBucketName(value)
@@ -25,30 +30,48 @@ export default observer(function ImageManagement() {
   const imageSolutionStore = useInjection(ImageSolutionStore)
 
   useEffect(() => {
+    setLoading(true)
     imageSolutionStore.fetchBucketList().then(() => {
       onChange(imageSolutionStore.bucketNames[0])
-    })
+    }).finally(() => setLoading(false))
   }, [imageSolutionStore, onChange])
 
-  return selectedBucketName && (
-    <>
-      <MediaStyleDrawer
-        {...mediaStyleStore}
-        bucketName={selectedBucketName}
-        region={bucketStore?.getDetailsByName(selectedBucketName)?.region ?? ''}
-        onClose={mediaStyleStore.handleClose}
-        key={`${selectedBucketName}-MediaStyleDrawer`}
+  if (loading) {
+    return (
+      <Loading
+        className="absolution-center"
+        loading={loading}
       />
+    )
+  }
 
-      <SelectBucket
-        value={selectedBucketName}
-        onChange={onChange}
-      />
-      <ObjectManage
-        bucketName={selectedBucketName}
-        isUploadModalOpen={false}
-        key={`${selectedBucketName}-ObjectManage`}
-      />
-    </>
+  if (selectedBucketName) {
+    return (
+      <>
+        <MediaStyleDrawer
+          {...mediaStyleStore}
+          bucketName={selectedBucketName}
+          region={bucketStore?.getDetailsByName(selectedBucketName)?.region ?? ''}
+          onClose={mediaStyleStore.handleClose}
+          key={`${selectedBucketName}-MediaStyleDrawer`}
+        />
+
+        <SelectBucket
+          value={selectedBucketName}
+          onChange={onChange}
+        />
+        <ObjectManage
+          bucketName={selectedBucketName}
+          isUploadModalOpen={false}
+          key={`${selectedBucketName}-ObjectManage`}
+        />
+      </>
+    )
+  }
+
+  return (
+    <div className="absolution-center">
+      <Empty />
+    </div>
   )
 })
