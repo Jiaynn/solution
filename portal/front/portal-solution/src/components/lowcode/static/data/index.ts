d28de0d43a7edd4e04a0350f2a29f75d0c3814ed
Jiaynn <73053080+Jiaynn@users.noreply.github.com>
2023-03-24 10:45:23 +0800

    fix(portal-solution): 修改文案 (#147)

--- portal/front/portal-solution/src/components/lowcode/static/data/index.ts
+++ portal/front/portal-solution/src/components/lowcode/static/data/index.ts
@@ -22,7 +22,7 @@ export const lists = [
     type: 0,
     title: '互动营销',
     img: 'https://demo-qnrtc-files.qnsdk.com/lowcode/yingixao.png',
-    desc: '七牛互动营销解决方案覆盖娱乐互动直播、电商直播带货、语聊房、互动教育等多应用场景，基于七牛云音视频、AI 智能算法和网络等先进技术，提供易接入、强扩展、高效部署和覆盖多场景的音视频服务，助力企业快速搭建高品质的专属音视频互动营销业务平台。',
+    desc: '覆盖娱乐互动直播、电商直播带货、语聊房、互动教育等多应用场景，基于七牛云音视频、AI 智能算法和网络，提供易接入、强扩展、高效部署的音视频服务，助力企业快速搭建高品质的专属音视频互动营销业务平台。',
     link_console: 'https://portal.qiniu.com/solutions/image/configuration/step/1?shouldCreateBucket=false&configurationState=true',
     link_more: 'https://www.qiniu.com/solutions/ent-live'
   },
@@ -31,7 +31,7 @@ export const lists = [
     type: 0,
     title: '统一消息营销触达',
     img: 'https://demo-qnrtc-files.qnsdk.com/lowcode/sms.png',
-    desc: '七牛云统一消息触达解决方案提供了包括短信、5G消息、微信、钉钉、客户端APP等多种消息触达客户通道，支持预设消息内容与变量，规范消息格式； 提供推送统计报表、消息历史报表、用户触达分析，整合各通道的推送统计结果，从渠道、通道、用户多维度分析转换率，以进行针对性促活。',
+    desc: '提供了包括短信、5G消息、微信、钉钉、客户端APP等多种消息触达客户通道，支持预设消息内容与变量，规范消息格式； 提供推送统计报表，从渠道、通道、用户多维度分析转换率，以进行针对性促活。',
     link_console: 'https://portal.qiniu.com/solutions/message',
     link_more: 'https://www.qiniu.com/solutions/ent-live'
   },
@@ -40,7 +40,7 @@ export const lists = [
     type: 0,
     title: '企业直播',
     img: 'https://demo-qnrtc-files.qnsdk.com/lowcode/zhibo.png',
-    desc: '七牛云企业直播解决方案，覆盖营销、带货、企业培训、活动直播等场景。无需开发即可使用，帮助企业快速集成和接入直播服务，支持与企业自有的会员系统、商城系统进行对接。丰富的互营销互动及数据分析能力，帮助企业实现内容生产、直播数据与流量三方面的闭环。 ',
+    desc: '覆盖营销、带货、企业培训、活动直播等场景；无需开发即可使用，快速集成和接入直播服务，支持与企业自有的会员系统、商城系统对接；丰富的互营销互动及数据分析能力，实现内容生产、直播数据与流量三方面的闭环。',
     link_console: 'https://live.youinsh.com/livestream/center?type=1&asideType=4',
     link_more: 'https://www.qiniu.com/solutions/ent-live'
   }
