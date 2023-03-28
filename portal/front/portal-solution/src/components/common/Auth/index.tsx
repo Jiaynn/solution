dc9db6b442dfcabb479d8880300c085a1194a126
Spencer17x <1253478653@qq.com>
2023-03-28 16:55:07 +0800

    feat(portal-solution): 项目列表页和创建项目交互

--- portal/front/portal-solution/src/components/common/Auth/index.tsx
+++ portal/front/portal-solution/src/components/common/Auth/index.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useInjection } from 'qn-fe-core/di'
 import { RouterStore } from 'qn-fe-core/router'
 
+import { useRequest } from 'ahooks'
+
 import { CommonApi } from 'apis/common'
 import { lowcodePath } from 'utils/router'
-import { isElectron } from 'constants/is'
 
 interface Props {
   children: React.ReactNode;
@@ -14,23 +15,13 @@ export const Auth: React.FC<Props> = props => {
   const { children } = props
   const commonApi = useInjection(CommonApi)
   const routerStore = useInjection(RouterStore)
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => {
-    setLoading(true)
-    commonApi.getLiveWhitelistCheck().then(result => {
-      setLoading(false)
-      if (!result.res) {
-        routerStore.push(`${lowcodePath}/prompt`)
-        return
-      }
-      if (isElectron) {
-        routerStore.push(`${lowcodePath}/welcome`)
-      }
-    })
-  }, [commonApi, routerStore])
+  const { loading } = useRequest(() => commonApi.getLiveWhitelistCheck().then(result => {
+    if (!result.res) {
+      routerStore.push(`${lowcodePath}/prompt`)
+    }
+  }))
 
   return (
-    loading ? null : <>{children}</>
+    loading ? <div>loading...</div> : <>{children}</>
   )
 }
