a68681d084dbc04c47cd5cd88c246109a71d4843
Spencer17x <1253478653@qq.com>
2023-03-30 11:47:04 +0800

    fix(portal-solution): 修复postMessage消息通信监听

--- portal/front/portal-solution/src/components/InteractMarketing/common/DownloadModal/index.tsx
+++ portal/front/portal-solution/src/components/InteractMarketing/common/DownloadModal/index.tsx
@@ -70,20 +70,20 @@ export default observer(function DownloadModal(props: DownloadModalProps) {
   }
 
   const onDownload = () => {
-    window.top?.electronBridgeApi.getDownloadStatus((_, result) => {
-      if (result.code === 0) {
-        setLoading1(true)
-      }
-      if (result.code === 1) {
-        setLoading1(false)
-      }
-    })
-
-    const a = document.createElement('a')
-    a.href =
-      'https://demo-qnrtc-files.qnsdk.com/solutions/portal/temp/1c90840b9344484c8c1e1398724f67efc281772a5fd62171b86795532289fc89/1680005098/droid_qlive_demo.zip'
-    a.download = 'droid_qlive_demo'
-    a.click()
+    // window.top?.electronBridgeApi.getDownloadStatus((_, result) => {
+    //   if (result.code === 0) {
+    //     setLoading1(true)
+    //   }
+    //   if (result.code === 1) {
+    //     setLoading1(false)
+    //   }
+    // })
+    //
+    // const a = document.createElement('a')
+    // a.href =
+    //   'https://demo-qnrtc-files.qnsdk.com/solutions/portal/temp/1c90840b9344484c8c1e1398724f67efc281772a5fd62171b86795532289fc89/1680005098/droid_qlive_demo.zip'
+    // a.download = 'droid_qlive_demo'
+    // a.click()
 
     onCreateProject()
   }
