a54618beedcfbf5bcfe9888d13746645ab8c61a0
Spencer17x <1253478653@qq.com>
2023-04-03 11:11:52 +0800

    fix(portal-solution): 功能预览页新增切换账号+路由NotFound+背景图

--- portal/front/portal-solution/src/components/lowcode/Prompt/index.tsx
+++ portal/front/portal-solution/src/components/lowcode/Prompt/index.tsx
@@ -1,15 +1,24 @@
 import React, { useState } from 'react'
 import './style.less'
-import { Button, Carousel, Divider } from 'react-icecream-2'
+import { Button, Carousel, Divider, Link } from 'react-icecream-2'
+
+import { useInjection } from 'qn-fe-core/di'
+import { UserInfoStore } from 'portal-base/user/account'
 
 import { LowcodeModal } from '../common/Modal'
 
 const prefixCls = 'lowcode-prompt'
 export const LowcodePrompt = () => {
+  const userInfoStore = useInjection(UserInfoStore)
   const [visible, setVisible] = useState(false)
   function handleCancel() {
     setVisible(false)
   }
+
+  const onToggleUser = () => {
+    userInfoStore.signOutAndGoSignIn(window.location.href)
+  }
+
   return (
     <div className={`${prefixCls}`}>
       <div className={`${prefixCls}-container`}>
@@ -22,7 +31,10 @@ export const LowcodePrompt = () => {
           </Carousel>
         </div>
 
-        <div className={`${prefixCls}-container-desc`}>您当前不是受邀用户，暂时无法使用本产品，请联系客服。</div>
+        <div className={`${prefixCls}-container-desc`}>
+          <span>您当前不是受邀用户，暂时无法使用本产品，请联系客服。</span>
+          <Link onClick={onToggleUser}>切换账号</Link>
+        </div>
         <div className={`${prefixCls}-container-btns`}>
           <Button type="primary" className={`${prefixCls}-container-btns-btn`} onClick={() => setVisible(true)}>联系客服</Button>
           <LowcodeModal visible={visible} handleCancel={() => handleCancel()} />
